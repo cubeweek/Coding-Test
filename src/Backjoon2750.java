fbeea3441154abf24f9192b2508320e8df26d1ea
CARRIE <61146269+cubeweek@users.noreply.github.com>
2025-10-15 01:46:45 +0900

    First Commit!

---
+++ src/Backjoon2750.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+// 실제 문제는 Main 클래스로 바꿔서 제출해야 함!
+public class Backjoon2750 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) arr[i] = sc.nextInt();
+
+        Arrays.sort(arr);
+
+        for (int num : arr) System.out.println(num);
+    }
+}

614fe7328c5e6a2eadf9799e670030d1aafd0017
Carrie <cubeweek@gmail.com>
2025-11-07 01:12:42 +0900

    25/11/06 Commit

---
+++ src/problem/gold/BJ2493.kt
@@ -0,0 +1,23 @@
+package problem.gold
+
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.util.*
+
+fun main(args: Array<String>) {
+    val br = BufferedReader(InputStreamReader(System.`in`))
+    val towerCnt = br.readLine().toInt()
+    val tower = br.readLine().split(" ").map { it.toInt() }
+    val stack = ArrayDeque<Int>()
+    val answer = IntArray(towerCnt)
+
+    tower.forEachIndexed { i, t ->
+        while (stack.isNotEmpty() && tower[stack.peek()] < t) stack.pop()
+        if (stack.isNotEmpty()) answer[i] = stack.peek() + 1
+        stack.push(i)
+    }
+
+    print(answer.joinToString(" "))
+
+    br.close()
+}

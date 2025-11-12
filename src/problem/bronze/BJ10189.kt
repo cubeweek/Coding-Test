a3ac8dba8c968f9d4d6b1bd5c4d80174ec3de165
Carrie <cubeweek@gmail.com>
2025-11-12 23:58:25 +0900

    25/11/12 Commit

---
+++ src/problem/bronze/BJ10189.kt
@@ -0,0 +1,8 @@
+package problem.bronze
+
+fun main (args: Array<String>) {
+    println("#  # #### #### #  #")
+    println("#### #  # #  # # #")
+    println("#### #  # #  # # #")
+    println("#  # #### #### #  #")
+}
---
+++ src/problem/silver/BJ2839.kt
@@ -0,0 +1,22 @@
+package problem.silver
+
+import java.io.BufferedReader
+import java.io.InputStreamReader
+
+fun main(args: Array<String>) {
+    val sugar = readln().toInt()
+
+//    var answer = -1
+//
+//    if (sugar % 5 == 0) answer = sugar / 5
+//    else {
+//        var (fiveCnt, threeCnt)
+//        var now
+//        while (fiveCnt * 5 < sugar) {
+//
+//        }
+//    }
+//
+//
+//    print(answer)
+}

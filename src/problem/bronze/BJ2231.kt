b43cbbf88c65ab0f068139715ed880cff90557d8
Carrie <cubeweek@gmail.com>
2025-11-22 23:20:24 +0900

    25/11/22 Commit

--- src/problem/bronze/BJ2231.kt
+++ src/problem/bronze/BJ2231.kt
@@ -1,21 +1,20 @@
 package problem.bronze
 
-import java.io.BufferedReader
-
 fun main(args: Array<String>) {
     val br = System.`in`.bufferedReader()
     val target = br.readLine().toInt()
     val start = target - 9 * target.toString().length
     var answer = 0
-    for (i in start..target) {
+    for (i in start until target) {
         var calc = i
-        var loading = i
-        while (calc > 0) {
-            calc += loading % 10
-            loading /= 10
+        var temp = i
+        while (temp > 0) {
+            calc += temp % 10
+            temp /= 10
         }
+
         if (calc == target) {
-            answer = calc
+            answer = i
             break
         }
     }
---
+++ src/problem/bronze/BJ2292.kt
@@ -0,0 +1,14 @@
+package problem.bronze
+
+fun main(args: Array<String>) {
+    var target = readln().toInt()
+    var calcHexagon = 6
+    var answer = 1
+
+    while (target > 1) {
+        target -= calcHexagon
+        calcHexagon += 6
+        answer++
+    }
+    println(answer)
+}

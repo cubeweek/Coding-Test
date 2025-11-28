bc2052b104f196ba9deb4a5ac5616a85a2b158fe
Carrie <cubeweek@gmail.com>
2025-11-28 23:50:17 +0900

    25/11/28 Commit

---
+++ src/problem/silver/BJ4714.kt
@@ -0,0 +1,20 @@
+package problem.silver
+
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.lang.StringBuilder
+
+fun main(args :Array<String>) {
+    val br = BufferedReader(InputStreamReader(System.`in`))
+    val sb = StringBuilder()
+
+    while (true) {
+        val earthWeight = br.readLine().toDouble()
+        if (earthWeight < 0) break
+        sb.appendLine("Objects weighing " + String.format("%.2f", earthWeight) + " on Earth will weigh " + String.format("%.2f", earthWeight * 0.167) + " on the moon.")
+
+    }
+    println(sb.toString())
+
+    br.close()
+}

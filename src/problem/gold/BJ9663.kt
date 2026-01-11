72a83e1baeb7bd9f834434372a118f76fd52d1f4
Carrie <cubeweek@gmail.com>
2026-01-11 23:51:53 +0900

    26/01/11 Commit

--- src/problem/gold/BJ9663.kt
+++ src/problem/gold/BJ9663.kt
@@ -1,39 +1,38 @@
 package problem.gold
 
-import java.lang.StrictMath.abs
-
 fun main (args: Array<String>) {
     val nq = readln().toInt()
     var answerCnt = 0
-    val board = IntArray(nq) { -1 }
 
-    fun isChecked(target: Int, pos: Int): Boolean {
-        for (i in 0 until pos) {
-            // 같은 줄에 있으면 안됨!
-            if (board[i] == target) return true
+    // 공격 불가 라인(세로, 대각선2) 기록
+    val line = BooleanArray(nq) // 세로줄
+    val diag1 = BooleanArray(2 * nq) // ↙
+    val diag2 = BooleanArray(2 * nq) // ↘
 
-            // 대각에 있어도 안됨!
-            if (abs(board[i] - target) == abs(i - pos)) return true
-        }
-        return false
-    }
-
-    fun findNoAttack(place: Int) {
-        if (place == nq) {
+    fun findNoAttack(row: Int) {
+        if (row == nq) {
             ++answerCnt
             return
         }
 
         for (i in 0 until nq) {
-            if (!isChecked(i, place)) {
-                board[place] = i
-                findNoAttack(place + 1)
-                board[place] = -1
-            }
+            val p1 = i + row
+            val p2 = row - i + nq
+
+            if (line[i] || diag1[p1] || diag2[p2]) continue
+
+            line[i] = true
+            diag1[p1] = true
+            diag2[p2] = true
+
+            findNoAttack(row + 1)
+
+            line[i] = false
+            diag1[p1] = false
+            diag2[p2] = false
         }
     }
+
     findNoAttack(0)
-
-
     println(answerCnt)
 }

1b928e29877f99cd3c86cc55f9c0165d1fe5cbdd
Cubeweek <61146269+cubeweek@users.noreply.github.com>
2026-01-29 23:25:03 +0900

    BJ2338.kt 만들기

---
+++ src/problem/baekjoon/bronze/b5/BJ2338.kt
@@ -0,0 +1,9 @@
+import java.math.BigInteger
+
+fun main(args: Array<String>) {
+    val a = BigInteger(readln())
+    val b = BigInteger(readln())
+    println(a.add(b))
+    println(a.subtract(b))
+    println(a.multiply(b))
+}

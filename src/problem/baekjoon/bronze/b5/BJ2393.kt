d5ebc877e7fd0ff172d70a60c56bf3cf7eba84f4
Carrie <cubeweek@gmail.com>
2026-02-01 20:30:37 +0900

    26/02/01 Commit

---
+++ src/problem/baekjoon/bronze/b5/BJ2393.kt
@@ -0,0 +1,17 @@
+package problem.baekjoon.bronze.b5
+
+fun main(args: Array<String>) {
+    println("  ___  ___  ___")
+    println("  | |__| |__| |")
+    println("  |           |")
+    println("   \\_________/")
+    println("    \\_______/")
+    println("     |     |")
+    println("     |     |")
+    println("     |     |")
+    println("     |     |")
+    println("     |_____|")
+    println("  __/       \\__")
+    println(" /             \\")
+    println("/_______________\\")
+}

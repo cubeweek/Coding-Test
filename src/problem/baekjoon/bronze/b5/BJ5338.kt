79e61bce3239b76a27beca14fd35b4f51d567615
Carrie <cubeweek@gmail.com>
2026-02-14 23:59:29 +0900

    26/02/14 Commit

---
+++ src/problem/baekjoon/bronze/b5/BJ5338.kt
@@ -0,0 +1,9 @@
+package problem.baekjoon.bronze.b5
+
+fun main (args: Array<String>) {
+    println("       _.-;;-._")
+    println("'-..-'|   ||   |")
+    println("'-..-'|_.-;;-._|")
+    println("'-..-'|   ||   |")
+    println("'-..-'|_.-''-._|")
+}

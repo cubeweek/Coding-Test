b918bdb76ba74b7fedd2d49ada9275fb214fa092
Carrie <cubeweek@gmail.com>
2026-02-19 23:56:44 +0900

    26/02/19 Commit

---
+++ src/problem/baekjoon/bronze/b5/BJ10718.kt
@@ -0,0 +1,7 @@
+package problem.baekjoon.bronze.b5
+
+fun main(args: Array<String>) {
+    repeat(2) {
+        println("강한친구 대한육군")
+    }
+}
---
+++ src/problem/baekjoon/bronze/b5/BJ30007.kt
@@ -0,0 +1,51 @@
+package problem.baekjoon.bronze.b5
+
+object FastReader {
+    val buffer = ByteArray(1 shl 16)
+    var lim = 0
+    var cur = 0
+
+    @Suppress("NOTHING_TO_INLINE")
+    private inline fun readByte(): Int {
+        if (cur >= lim) {
+            lim = System.`in`.read(buffer)
+            if (lim <= 0) return -1
+            cur = 0
+        }
+        return buffer[cur++].toInt()
+    }
+
+    fun nextInt(): Int {
+        var c = readByte()
+        while (c in 0..32) c = readByte()
+        if (c == -1) return 0
+
+        var isNeg = false
+        if (c == 45) {
+            isNeg = true
+            c = readByte()
+        }
+
+        var res = 0
+        while (c in 48..57) {
+            res = (res shl 3) + (res shl 1) + (c and 15)
+            c = readByte()
+        }
+        return if(isNeg) -res else res
+    }
+}
+
+fun main(args: Array<String>) {
+    val fs = FastReader
+    val n = fs.nextInt()
+
+    val sb = StringBuilder()
+    repeat(n) {
+        val a = fs.nextInt()
+        val b = fs.nextInt()
+        val x = fs.nextInt()
+
+        sb.append(a * (x - 1) + b).append("\n")
+    }
+    println(sb)
+}
